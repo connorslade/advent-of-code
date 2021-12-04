d33cfbe674e100aceed9182d34a5def5bc313cf9
Connor Slade <connor@connorcode.com>
2021-12-04 11:00:55 -0500

    🥅 Day 4

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -3,5 +3,11 @@ use crate::common::Solution;
 pub mod day_01;
 pub mod day_02;
 pub mod day_03;
+pub mod day_04;
 
-pub const ALL: [&dyn Solution; 3] = [&day_01::Day01 {}, &day_02::Day02 {}, &day_03::Day03 {}];
+pub const ALL: [&dyn Solution; 4] = [
+    &day_01::Day01 {},
+    &day_02::Day02 {},
+    &day_03::Day03 {},
+    &day_04::Day04 {},
+];
