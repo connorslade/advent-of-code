5ed19ae1c3ad2105479c9d36267e05c6ab7d6287
Connor Slade <connor@connorcode.com>
2021-12-09 10:28:15 -0500

    🪛 Fixup Day 9 - A

--- src/solutions/day_09.rs
+++ src/solutions/day_09.rs
@@ -4,7 +4,7 @@ pub struct Day09 {}
 
 impl Solution for Day09 {
     fn name(&self) -> String {
-        "".to_owned()
+        "Smoke Basin".to_owned()
     }
 
     fn part_a(&self) -> String {
@@ -15,32 +15,33 @@ impl Solution for Day09 {
     }
 
     fn part_b(&self) -> String {
-        let data = parse(common::load("09"));
+        let data = common::load("09");
 
         "".to_string()
     }
 }
 
 fn parse(inp: String) -> Vec<Vec<u32>> {
-    let mut out = Vec::new();
-
-    for i in inp.lines() {
-        out.push(
-            i.chars()
-                .map(|x| x.to_digit(10).unwrap())
-                .collect::<Vec<u32>>(),
-        );
-    }
-
-    out
+    inp.lines()
+        .map(|x| x.chars().map(|f| f.to_digit(10).unwrap()).collect())
+        .collect()
 }
 
 fn lowest(inp: Vec<Vec<u32>>) -> Vec<u32> {
-    let mut out = Vec::new();
-
-    for i in 0..inp.len() {
-        for j in 0..inp[i].len() {}
-    }
-
-    out
+    inp.iter()
+        .enumerate()
+        .flat_map(|(i, line)| {
+            let inp = &inp;
+            line.iter().enumerate().filter_map(move |(j, &h)| {
+                if (i == 0 || h < inp[i - 1][j])
+                    && (i == inp.len() - 1 || h < inp[i + 1][j])
+                    && (j == 0 || h < inp[i][j - 1])
+                    && (j == line.len() - 1 || h < inp[i][j + 1])
+                {
+                    return Some(h);
+                }
+                None
+            })
+        })
+        .collect::<Vec<u32>>()
 }
