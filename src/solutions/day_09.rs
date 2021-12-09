bd0c0c1d369df0a27cfd8a93514e5847ff160228
Connor Slade <connor@connorcode.com>
2021-12-09 15:58:59 -0500

    🗽 Day 9 B

--- src/solutions/day_09.rs
+++ src/solutions/day_09.rs
@@ -15,9 +15,10 @@ impl Solution for Day09 {
     }
 
     fn part_b(&self) -> String {
-        let data = common::load("09");
+        let data = parse(common::load("09"));
+        let basins = basins(data);
 
-        "".to_string()
+        basins.iter().rev().take(3).product::<u32>().to_string()
     }
 }
 
@@ -44,4 +45,31 @@ fn lowest(inp: Vec<Vec<u32>>) -> Vec<u32> {
             })
         })
         .collect::<Vec<u32>>()
+}
+
+fn basins(mut inp: Vec<Vec<u32>>) -> Vec<u32> {
+    let mut basins = Vec::new();
+    for i in 0..inp.len() {
+        for j in 0..inp[0].len() {
+            if inp[i][j] < 9 {
+                basins.push(basin(&mut inp, j, i));
+            }
+        }
+    }
+
+    basins.sort_unstable();
+    basins
+}
+
+fn basin(map: &mut Vec<Vec<u32>>, x: usize, y: usize) -> u32 {
+    map[y][x] = 9;
+    [(0, -1), (0, 1), (-1, 0), (1, 0)]
+        .iter()
+        .map(|(xx, yy)| ((x as isize + xx) as usize, (y as isize + yy) as usize))
+        .fold(1, |inc, (x, y)| {
+            if map.get(y).and_then(|l| l.get(x)).map(|&n| n < 9) == Some(true) {
+                return inc + basin(map, x, y);
+            }
+            inc
+        })
 }
