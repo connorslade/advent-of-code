c30a83adaa8e727e7e5a2d914a4b45e1b1a4c7f2
Connor Slade <connor@connorcode.com>
2022-12-05 13:03:01 -0500

    Day 5: Cleanup

--- src/solutions/year_2022/day_05.rs
+++ src/solutions/year_2022/day_05.rs
@@ -8,20 +8,20 @@ impl Solution for Day05 {
     }
 
     fn part_a(&self) -> String {
-        let raw = problem::load(2022, 5).replace("\r", "");
+        let raw = problem::load(2022, 5);
         process(&raw, true)
     }
 
     fn part_b(&self) -> String {
-        let raw = problem::load(2022, 5).replace("\r", "");
+        let raw = problem::load(2022, 5);
         process(&raw, false)
     }
 }
 
-// TODO: Clean this up
 fn process(raw: &str, part: bool) -> String {
-    let (cretes, orders) = raw.split_once("\n\n").unwrap();
-    let mut crates = parse_crates(cretes);
+    let raw = raw.replace('\r', "");
+    let (crates, orders) = raw.split_once("\n\n").unwrap();
+    let mut crates = parse_crates(crates);
 
     for i in orders.trim().lines() {
         let parts = i.split_whitespace().collect::<Vec<_>>();
@@ -29,44 +29,29 @@ fn process(raw: &str, part: bool) -> String {
         let from = parts[3].parse::<usize>().unwrap() - 1;
         let to = parts[5].parse::<usize>().unwrap() - 1;
 
+        let count = crates[from].len() - count..;
+        let mut working = crates[from].drain(count).collect::<Vec<_>>();
         if part {
-            for _ in 0..count {
-                let old = crates[from].remove(0);
-                crates[to].insert(0, old);
-            }
-            continue;
+            working.reverse();
         }
 
-        let mut working = Vec::new();
-        for _ in 0..count {
-            working.push(crates[from].remove(0));
-        }
-
-        for i in working.iter().rev() {
-            crates[to].insert(0, *i);
-        }
+        crates[to].extend(working);
     }
 
-    let mut out = String::new();
-    for i in crates.iter().filter(|x| !x.is_empty()) {
-        out.push(i[0]);
-    }
-
-    out
+    crates
+        .into_iter()
+        .filter(|x| !x.is_empty())
+        .map(|mut x| x.pop().unwrap())
+        .collect()
 }
 
 fn parse_crates(raw: &str) -> Vec<Vec<char>> {
-    let mut out = vec![Vec::new(); 9];
+    let raw_len = raw.lines().next().unwrap().len() + 1;
+    let mut out = vec![Vec::new(); raw_len / 4];
 
     for i in raw.lines().filter(|x| !x.starts_with(" 1")) {
-        for j in 0..9 {
-            if let Some(x) = i.chars().nth(1 + j * 4) {
-                if x.is_whitespace() {
-                    continue;
-                }
-
-                out[j].push(x);
-            }
+        for (ji, i) in i.chars().enumerate().filter(|x| x.1.is_ascii_uppercase()) {
+            out[(ji as f32 / 4.).ceil() as usize - 1].insert(0, i);
         }
     }
 
