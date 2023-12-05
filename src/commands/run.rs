cbe3d61c9031fcce0dac99630a9944497db752de
Connor Slade <connor@connorcode.com>
2023-12-05 16:35:40 -0500

    [CLI] Better error handing when input not found

--- src/commands/run.rs
+++ src/commands/run.rs
@@ -1,27 +1,34 @@
-use std::{fs, time::Instant};
+use std::{borrow::Cow, fs, path::PathBuf, time::Instant};
 
-use anyhow::{Context, Result};
+use anyhow::{bail, Context, Result};
 use common::{human_time, Part};
 
 use crate::{args::RunArgs, get_year};
 
 pub fn run(cmd: &RunArgs) -> Result<()> {
-    let day = cmd.day.saturating_sub(1);
-
     let solutions = get_year(cmd.year);
     let solution = solutions
-        .get(day as usize)
-        .with_context(|| format!("No solution for day {} in year {}", day, cmd.year))?;
+        .get(cmd.day.saturating_sub(1) as usize)
+        .with_context(|| format!("No solution for day {} in year {}", cmd.day, cmd.year))?;
 
     println!(
         "[*] Running: {} ({})",
         solution.name(),
         cmd.part.to_string().to_uppercase()
     );
-    let input = match &cmd.input {
-        Some(path) => fs::read_to_string(path)?.trim().replace('\r', ""),
-        None => common::load(cmd.year, day + 1)?,
+
+    let path = match &cmd.input {
+        Some(path) => Cow::Borrowed(path),
+        None => {
+            let path = PathBuf::from(format!("data/{}/{:02}.txt", cmd.year, cmd.day));
+            if !path.exists() {
+                bail!("Default input file does not exist. Use --input to specify a path.");
+            }
+            Cow::Owned(path)
+        }
     };
+
+    let input = fs::read_to_string(&*path)?.trim().replace('\r', "");
 
     let start = Instant::now();
     let out = match cmd.part {
