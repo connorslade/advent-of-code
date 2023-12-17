16ae4d9042a9849901a35a4fd90b7c5d478f2c45
Connor Slade <connor@connorcode.com>
2023-12-17 02:47:24 -0500

    [2023] Cleanup day 17

--- aoc_2023/src/day_17.rs
+++ aoc_2023/src/day_17.rs
@@ -7,147 +7,59 @@ type Pos = Vec2<usize>;
 
 pub struct Day17;
 
-// this is quite the spaghetti
 impl Solution for Day17 {
     fn name(&self) -> &'static str {
         "Clumsy Crucible"
     }
 
     fn part_a(&self, input: &str) -> Answer {
-        let board = parse(input);
-
-        #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-        struct State {
-            pos: Pos,
-            facing: Direction,
-            turn_distance: u8,
-            steps: u32,
-        }
-
-        let start = State {
-            pos: vector!(0, 0),
-            facing: Direction::Right,
-            turn_distance: 0,
-            steps: 0,
-        };
-
-        let path = pathfinding::directed::dijkstra::dijkstra(
-            &start,
-            |x| {
-                let mut next = Vec::new();
-
-                // Straight
-                if x.turn_distance < 2 {
-                    if let Some(next_pos) = x.facing.advance(x.pos) {
-                        if next_pos.x() < board.len() && next_pos.y() < board[0].len() {
-                            let cell = board[next_pos.x()][next_pos.y()] as u32;
-                            next.push((
-                                State {
-                                    pos: next_pos,
-                                    facing: x.facing,
-                                    turn_distance: x.turn_distance + 1,
-                                    steps: x.steps + cell,
-                                },
-                                cell,
-                            ));
-                        }
-                    }
-                }
-
-                // Left
-                if let Some(next_facing) = x.facing.turn_left().advance(x.pos) {
-                    if next_facing.x() < board.len() && next_facing.y() < board[0].len() {
-                        let cell = board[next_facing.x()][next_facing.y()] as u32;
-                        next.push((
-                            State {
-                                pos: next_facing,
-                                facing: x.facing.turn_left(),
-                                turn_distance: 0,
-                                steps: x.steps + cell,
-                            },
-                            cell,
-                        ));
-                    }
-                }
-
-                // Right
-                if let Some(next_facing) = x.facing.turn_right().advance(x.pos) {
-                    if next_facing.x() < board.len() && next_facing.y() < board[0].len() {
-                        let cell = board[next_facing.x()][next_facing.y()] as u32;
-                        next.push((
-                            State {
-                                pos: next_facing,
-                                facing: x.facing.turn_right(),
-                                turn_distance: 0,
-                                steps: x.steps + cell,
-                            },
-                            cell,
-                        ));
-                    }
-                }
-
-                next
-            },
-            |x| x.pos == vector!(board.len() - 1, board[0].len() - 1),
-        );
-
-        path.unwrap().1.into()
+        pathfind(parse(input), 1, 3).into()
     }
 
     fn part_b(&self, input: &str) -> Answer {
-        let board = parse(input);
-        pathfind(&board, 4, 10).into()
+        pathfind(parse(input), 4, 10).into()
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-struct State {
-    pos: Pos,
-    facing: Direction,
-    turn_distance: u8,
-    steps: u32,
-}
-
-fn pathfind(board: &[Vec<u8>], lower: u8, upper: u8) -> u32 {
-    let mut res = u32::MAX;
+fn pathfind(board: Vec<Vec<u8>>, min_dist: u8, max_dist: u8) -> u32 {
     let mut queue = VecDeque::new();
     let mut visited = HashMap::new();
+    let mut res = u32::MAX;
 
     let end = vector!(board[0].len() - 1, board.len() - 1);
     for dir in [Direction::Down, Direction::Right] {
-        let state = (vector!(0, 0), dir, 1);
-        queue.push_back((state, 0));
+        let state = State::new(vector!(0, 0), dir, 1);
+        queue.push_back((0, state));
         visited.insert(state, 0);
     }
 
-    while let Some(((pos, facing, turn_distance), cost)) = queue.pop_front() {
-        let mut push = |dir: Direction, turn_distance: u8| {
-            if let Some(next) = dir.advance(pos) {
-                if next.y() < board.len() && next.x() < board[0].len() {
-                    let state = (next, dir, turn_distance);
-                    let cell = board[next.y()][next.x()] as u32;
-                    let cost = cost + cell;
+    while let Some((cost, state)) = queue.pop_front() {
+        let mut push = |facing: Direction, turn_distance: u8| {
+            if let Some(pos) = facing.advance(state.pos) {
+                if pos.y() < board.len() && pos.x() < board[0].len() {
+                    let state = State::new(pos, facing, turn_distance);
+                    let cost = cost + board[pos.y()][pos.x()] as u32;
 
-                    if !visited.contains_key(&state) || visited[&state] > cost {
-                        queue.push_back((state, cost));
+                    if !visited.contains_key(&state) || visited.get(&state).unwrap() > &cost {
+                        queue.push_back((cost, state));
                         visited.insert(state, cost);
                     }
                 }
             }
         };
 
-        if pos == end && turn_distance >= 4 {
+        if state.pos == end && state.turn_distance >= min_dist {
             res = res.min(cost);
             continue;
         }
 
-        if turn_distance < upper {
-            push(facing, turn_distance + 1);
+        if state.turn_distance < max_dist {
+            push(state.facing, state.turn_distance + 1);
         }
 
-        if turn_distance >= lower {
-            push(facing.turn_left(), 1);
-            push(facing.turn_right(), 1);
+        if state.turn_distance >= min_dist {
+            push(state.facing.turn_left(), 1);
+            push(state.facing.turn_right(), 1);
         }
     }
 
@@ -159,6 +71,23 @@ fn parse(input: &str) -> Vec<Vec<u8>> {
         .lines()
         .map(|line| line.chars().map(|x| x as u8 - b'0').collect())
         .collect::<Vec<_>>()
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct State {
+    pos: Pos,
+    facing: Direction,
+    turn_distance: u8,
+}
+
+impl State {
+    fn new(pos: Pos, facing: Direction, turn_distance: u8) -> Self {
+        Self {
+            pos,
+            facing,
+            turn_distance,
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -205,14 +134,6 @@ mod test {
     use indoc::indoc;
 
     use super::Day17;
-
-    // const CASE: &str = indoc! {"
-    //     111111111111
-    //     999999999991
-    //     999999999991
-    //     999999999991
-    //     999999999991
-    // "};
 
     const CASE: &str = indoc! {"
         2413432311323
