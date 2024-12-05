f202acbe68da9bed1dd776bc69ad588aaa44e33e
Connor Slade <connor@connorcode.com>
2024-12-05 00:53:09 -0500

    [2024] Cleanup day 5

--- aoc_2024/src/day_05.rs
+++ aoc_2024/src/day_05.rs
@@ -5,43 +5,44 @@ use common::{solution, Answer};
 solution!("Print Queue", 5);
 
 fn part_a(input: &str) -> Answer {
-    let problem = Problem::parse(input);
+    let problem = PrintQueue::parse(input);
 
-    let mut sum = 0;
-    for i in 0..problem.updates.len() {
-        if problem.is_valid(i) {
-            let update = &problem.updates[i];
-            sum += update[update.len() / 2];
-        }
-    }
-
-    sum.into()
+    // For each list of pages in an update, sum the middle value of all valid
+    // ones.
+    (0..problem.updates.len())
+        .filter(|&x| problem.is_valid(x))
+        .map(|x| &problem.updates[x])
+        .map(|x| x[x.len() / 2])
+        .sum::<u32>()
+        .into()
 }
 
 fn part_b(input: &str) -> Answer {
-    let problem = Problem::parse(input);
+    let problem = PrintQueue::parse(input);
 
-    let mut sum = 0;
-    for i in 0..problem.updates.len() {
-        if !problem.is_valid(i) {
-            let update = problem.sort_pages(i);
-            sum += update[update.len() / 2];
-        }
-    }
-
-    sum.into()
+    // For each list of pages that are not correctly sorted, sort them then find
+    // the middle.
+    (0..problem.updates.len())
+        .filter(|&x| !problem.is_valid(x))
+        .map(|x| problem.sort_pages(x))
+        .map(|x| x[x.len() / 2])
+        .sum::<u32>()
+        .into()
 }
 
-struct Problem {
+struct PrintQueue {
     /// Maps a page to the pages that must come before it.
     rule_map: HashMap<u32, HashSet<u32>>,
     updates: Vec<Vec<u32>>,
 }
 
-impl Problem {
+impl PrintQueue {
     fn parse(input: &str) -> Self {
         let (rules, updates) = input.split_once("\n\n").unwrap();
 
+        // a|b => a comes before b
+        // For each rule stating that some page a comes before some page b, add
+        // a to the list of pages that come before b.
         let mut rule_map: HashMap<u32, HashSet<u32>> = HashMap::new();
         for (a, b) in rules.lines().map(|x| {
             let (a, b) = x.split_once('|').unwrap();
@@ -60,12 +61,16 @@ impl Problem {
 
     fn is_valid(&self, idx: usize) -> bool {
         let line = &self.updates[idx];
-        line.is_sorted_by(|a, b| self.rule_map[b].contains(a))
+        // A line is sorted if to the left of every page is a page that should be before it.
+        line.is_sorted_by(|a, b| self.rule_map.contains_key(b) && self.rule_map[b].contains(a))
     }
 
     fn sort_pages(&self, idx: usize) -> Vec<u32> {
         let mut line = self.updates[idx].clone();
-        line.sort_by(|a, b| self.rule_map[b].contains(a).cmp(&true));
+        // Just the same expression from before used by a sorting algo to put our pages in order!
+        line.sort_by(|a, b| {
+            (self.rule_map.contains_key(b) && self.rule_map[b].contains(a)).cmp(&true)
+        });
         line
     }
 }
