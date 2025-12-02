c261553803bc721cf2e4bf3969d8d6d50ec74c1a
Connor Slade <connor@connorcode.com>
2025-12-02 00:47:36 -0500

    [2025] Cleanup day 2 solution

--- aoc_2025/src/day_02.rs
+++ aoc_2025/src/day_02.rs
@@ -3,9 +3,45 @@ use common::{Answer, solution};
 solution!("Gift Shop", 2);
 
 fn part_a(input: &str) -> Answer {
+    count_invalid(input, |id| {
+        let digits = id.ilog10().div_ceil(2);
+        let mask = u64::pow(10, digits);
+        id % mask == id / mask
+    })
+    .into()
+}
+
+fn part_b(input: &str) -> Answer {
+    count_invalid(input, |id| {
+        let digits = id.ilog10() + 1;
+        'outer: for size in 1..=(digits / 2) {
+            let mask = u64::pow(10, size);
+            let repeated = id % mask;
+            if !digits.is_multiple_of(size) {
+                continue;
+            }
+
+            let mut id = id;
+            for _ in 0..(digits / size) {
+                if id % mask != repeated {
+                    continue 'outer;
+                }
+
+                id /= mask;
+            }
+
+            return true;
+        }
+
+        false
+    })
+    .into()
+}
+
+fn count_invalid(input: &str, is_invalid: fn(u64) -> bool) -> u64 {
     let mut count = 0;
 
-    for range in input.trim().split(',') {
+    for range in input.split(',') {
         let (start, end) = range.split_once('-').unwrap();
         let start = start.parse::<u64>().unwrap();
         let end = end.parse::<u64>().unwrap();
@@ -15,63 +51,12 @@ fn part_a(input: &str) -> Answer {
         }
     }
 
-    count.into()
-}
-
-fn part_b(input: &str) -> Answer {
-    let mut count = 0;
-
-    for range in input.trim().split(',') {
-        let (start, end) = range.split_once('-').unwrap();
-        let start = start.parse::<u64>().unwrap();
-        let end = end.parse::<u64>().unwrap();
-
-        for id in start..=end {
-            count += is_invalid2(id) as u64 * id;
-        }
-    }
-
-    count.into()
-}
-
-fn is_invalid(id: u64) -> bool {
-    let digits = (id.ilog10() + 1) / 2;
-    let mask = 10_u64.pow(digits);
-    id % mask == id / mask
-}
-
-fn is_invalid2(id: u64) -> bool {
-    let digits = id.ilog10() + 1;
-
-    'outer: for size in 1..=(digits / 2) {
-        let mask = 10_u64.pow(size);
-        let repeated = id % mask;
-        if (digits / size) * size != digits || repeated == 0 || repeated.ilog10() + 1 != size {
-            continue;
-        }
-
-        let mut id = id;
-        for _ in 0..(digits / size) {
-            if id % mask != repeated {
-                continue 'outer;
-            }
-
-            id /= mask;
-        }
-
-        return true;
-    }
-
-    false
+    count
 }
 
 #[cfg(test)]
 mod test {
-    use indoc::indoc;
-
-    const CASE: &str = indoc! {"
-        11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
-    "};
+    const CASE: &str = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
 
     #[test]
     fn part_a() {
