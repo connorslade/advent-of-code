4796f101f0397e596857e7d04f2a3510c60ebe56
Connor Slade <connor@connorcode.com>
2025-12-09 01:46:21 -0500

    [2025] Cleanup day 9

--- aoc_2025/src/day_09.rs
+++ aoc_2025/src/day_09.rs
@@ -7,10 +7,9 @@ use nd_vec::{Vec2, vector};
 solution!("Movie Theater", 9);
 
 fn part_a(input: &str) -> Answer {
-    parse(input)
-        .iter()
-        .combinations(2)
-        .map(|x| (x[0].x().abs_diff(x[1].x()) + 1) * (x[0].y().abs_diff(x[1].y()) + 1))
+    (parse(input).iter())
+        .tuple_combinations()
+        .map(|(a, b)| area(a, b))
         .max()
         .unwrap()
         .into()
@@ -20,60 +19,58 @@ fn part_b(input: &str) -> Answer {
     let red = parse(input);
     red.iter()
         .tuple_combinations()
-        .map(|(a, b)| {
-            (
-                (a.x().abs_diff(b.x()) + 1) * (a.y().abs_diff(b.y()) + 1),
-                (a, b),
-            )
-        })
+        .map(|(a, b)| (area(a, b), (a, b)))
         .sorted_by_key(|(area, _)| Reverse(*area))
         .find(|(_area, (a, b))| {
-            let min = vector!(a.x().min(b.x()), a.y().min(b.y()));
-            let max = vector!(a.x().max(b.x()), a.y().max(b.y()));
-
-            for (&α, &β) in red.iter().chain(iter::once(&red[0])).tuple_windows() {
-                let lmin = vector!(α.x().min(β.x()), α.y().min(β.y()));
-                let lmax = vector!(α.x().max(β.x()), α.y().max(β.y()));
-
-                if α.x() == β.x() {
-                    let x = β.x();
-                    if ((lmin.y() < min.y() && lmax.y() > min.y())
-                        || (lmin.y() < max.y() && lmax.y() > max.y())
-                        || (lmin.y() >= min.y() && lmax.y() <= max.y()))
-                        && x > min.x()
-                        && x < max.x()
-                    {
-                        return false;
-                    }
-                } else if α.y() == β.y() {
-                    let y = β.y();
-                    if ((lmin.x() < min.x() && lmax.x() > min.x())
-                        || (lmin.x() < max.x() && lmax.x() > max.x())
-                        || (lmin.x() >= min.x() && lmax.x() <= max.x()))
-                        && y > min.y()
-                        && y < max.y()
-                    {
-                        return false;
-                    }
-                }
-            }
-
-            true
+            let bounds = bounds(a, b);
+            !red.iter()
+                .chain(iter::once(&red[0]))
+                .tuple_windows()
+                .any(|line| intersecting_line(line, bounds))
         })
         .unwrap()
         .0
         .into()
 }
 
-// not: >1565730054
-
-fn parse(input: &str) -> Vec<Vec2<i64>> {
+fn parse(input: &str) -> Vec<Vec2<u64>> {
     (input.lines())
         .map(|x| {
             let (x, y) = x.split_once(',').unwrap();
             vector!(x.parse().unwrap(), y.parse().unwrap())
         })
         .collect::<Vec<_>>()
+}
+
+fn area(a: &Vec2<u64>, b: &Vec2<u64>) -> u64 {
+    (a.x().abs_diff(b.x()) + 1) * (a.y().abs_diff(b.y()) + 1)
+}
+
+fn bounds(a: &Vec2<u64>, b: &Vec2<u64>) -> (Vec2<u64>, Vec2<u64>) {
+    (
+        vector!(a.x().min(b.x()), a.y().min(b.y())),
+        vector!(a.x().max(b.x()), a.y().max(b.y())),
+    )
+}
+
+fn intersecting_line(
+    (la, lb): (&Vec2<u64>, &Vec2<u64>),
+    (min, max): (Vec2<u64>, Vec2<u64>),
+) -> bool {
+    let (lmin, lmax) = bounds(la, lb);
+
+    la.x() == lb.x() // horizontal line
+        && (((lmin.y() < min.y() && lmax.y() > min.y())
+            || (lmin.y() < max.y() && lmax.y() > max.y())
+            || (lmin.y() >= min.y() && lmax.y() <= max.y()))
+            && la.x() > min.x()
+            && la.x() < max.x())
+        || la.y() == lb.y() // vertical line
+            && (((lmin.x() < min.x() && lmax.x() > min.x())
+                || (lmin.x() < max.x() && lmax.x() > max.x())
+                || (lmin.x() >= min.x() && lmax.x() <= max.x()))
+                && la.y() > min.y()
+                && la.y() < max.y())
 }
 
 #[cfg(test)]
